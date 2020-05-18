547cab12811a0fc78b66a13889973d889138b380
0x212457 <65234039+0x212457@users.noreply.github.com>
2020-05-18 13:46:16 +0200

    Update CSGO-showsrcdslogs.sh

--- server212457/CSGO-showsrcdslogs.sh
+++ server212457/CSGO-showsrcdslogs.sh
@@ -1,2 +1,2 @@
-SERVER_HOMEDIR=/home/steam/csgo-servers/212457-1/csgo/log
-tail -f `find $SERVER_HOMEDIR -type f -name "csgo-srcds-*" | sort -n | tail -1`
+SERVER_HOMEDIR_LOG=/home/steam/csgo-servers/212457-1/csgo/log
+tail -f `find $SERVER_HOMEDIR_LOG -type f -name "csgo-srcds-*" | sort -n | tail -1`
