35f71f0fe553f281ff5ef5fab023135fbd16eaf3
0x212457 <65234039+0x212457@users.noreply.github.com>
2020-06-16 16:26:13 +0200

    Update watch-demo-files.sh

--- server212457/watch-demo-files.sh
+++ server212457/watch-demo-files.sh
@@ -2,7 +2,7 @@
 #This script watch the F/S to detect the end of a demo recording and create the direct download link to get it
 
 CSGOSRV_HOME_DIR=/home/steam/csgo-servers/212457-1/csgo/csgo
-CS_WEB_SITE=/var/www/cs.bl.lc
+CS_WEB_SITE=/var/www/cs.bl.lc/
 CS_WEB_ADOC_FILE=$CS_WEB_SITE/index.adoc
 CS_WEB_HTML_FILE=$CS_WEB_SITE/index.html
 CS_WEB_LOG_FILE=$CS_WEB_SITE/index.log
@@ -14,7 +14,7 @@ inotifywait -m $CSGOSRV_HOME_DIR -e close_write | while read path action file
                                                 ln -s ${path}${file} $CS_WEB_SITE
                                                 FILE_SIZE=`du -hL ${path}${file} | cut -f1`
                                                 echo -e " " >> $CS_WEB_ADOC_FILE
-                                                echo -e "http://cs.bl.lc/$file[$file, $FILE_SIZE]" >> $CS_WEB_ADOC_FILE
+                                                echo -e "${CS_WEB_SITE}${file}[$file, $FILE_SIZE]" >> $CS_WEB_ADOC_FILE
                                                 echo -e " " >> $CS_WEB_ADOC_FILE
 
                                                 asciidoctor -v -o $CS_WEB_HTML_FILE $CS_WEB_ADOC_FILE >> $CS_WEB_LOG_FILE 2>&1
